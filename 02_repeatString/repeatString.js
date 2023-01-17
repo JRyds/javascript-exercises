d3e367e8caa72951e4829afdaac690239d8ca953
JRyds <jpryding@gmail.com>
2023-01-17 10:28:21 +0100

    Exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,17 @@
-const repeatString = function() {
+const repeatString = function(str, num) {
+    let output = '';
+    let i = 0
 
-};
+    if (num < 0) {
+        return 'ERROR';
+    } else {
+        while (i < num) {
+            output = output + str;
+            i++
+        }
+        return output;
+    }
+}
 
 // Do not edit below this line
 module.exports = repeatString;
