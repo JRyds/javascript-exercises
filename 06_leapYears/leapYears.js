a275e7af91dba2d0dc7777d631b2e17ac49e1bcd
JRyds <jpryding@gmail.com>
2023-01-19 14:55:17 +0100

    Exercises 6 and 7 complete.

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,15 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    //return year % 4 === 0;
+    if (year % 4 !== 0) {
+        return false;
+    } else if (Number.isInteger(year / 100) && !Number.isInteger(year / 400) ) {
+        return false;
+    } else {
+        return true;
+    }
 
 };
+
 
 // Do not edit below this line
 module.exports = leapYears;
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,16 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(degrees) {
+  let rawConvert =  (degrees -32) * 5/9
+  return parseFloat(rawConvert.toFixed(1))
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(degrees) {
+  let rawConvert =  degrees * 9/5 + 32
+  return parseFloat(rawConvert.toFixed(1))
+
 };
 
+
+console.log(convertToFahrenheit(38))
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
