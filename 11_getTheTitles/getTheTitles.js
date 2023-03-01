2094bcb34f5e6949f469ff17a6285c722cf024d6
JRyds <jpryding@gmail.com>
2023-03-01 15:11:16 +0100

    test passed

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,9 @@
-const getTheTitles = function() {
+const getTheTitles = function(books) {
+// Use the map() method to create a new array of book titles
+    const titles = books.map(book => book.title);
 
+    // Return the titles array
+    return titles;
 };
 
 // Do not edit below this line
